3c6ccc939eec07ff7a716ec8ba598f64f12b19db
ThunderSpark91 <joerie.judong@gmail.com>
2022-06-27 17:56:27 +0200

    Commit 27/05/2022

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -9,5 +9,30 @@ function getAge(dateString) {
     return age;
 }
 
+var currentage = String(getAge('1991/07/26'));
+var agestring = currentage;
+var ageContainer = document.querySelector("#currentage");
 
-var currentage = getAge('26/07/1991')
+ageContainer.innerHTML += agestring;
+
+
+var counterContainer = document.querySelector("#website-counter");
+// var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+    visitCount = Number(visitCount) + 1;
+    localStorage.setItem("page_view", visitCount);
+} else {
+    visitCount = 1;
+    localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML += visitCount;
+
+// Adding onClick event listener
+// resetButton.addEventListener("click", () => {
+//   visitCount = 1;
+//   localStorage.setItem("page_view", 1);
+//   counterContainer.innerHTML = visitCount;
+// });
