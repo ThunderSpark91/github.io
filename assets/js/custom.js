b893e1c3b08df39edc067fdc826e0c9aa10fdfcd
ThunderSpark91 <joerie.judong@gmail.com>
2022-06-29 00:13:18 +0200

    28/06/2022 Contact update

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -16,7 +16,7 @@ var ageContainer = document.querySelector("#currentage");
 ageContainer.innerHTML += agestring;
 
 
-var counterContainer = document.querySelector("#website-counter");
+var counterContainer = document.querySelector(".website-counter");
 // var resetButton = document.querySelector("#reset");
 var visitCount = localStorage.getItem("page_view");
 
